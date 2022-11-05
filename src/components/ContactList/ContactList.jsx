7b4b432eb16c8c2591d868b5ed35dafe4e3596db
Volodymyr-Ogiychuk <volodymyr.ogiychuk@gmail.com>
2022-11-05 21:51:57 +0200

    Add updates with localStorage to App and ContList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,36 @@
 import PropTypes from 'prop-types'
 import s from './ContactList.module.css'
+import React, {Component} from 'react';
 
-const ContactList = ({ contacts, deleteContact }) => {  
-    return (
-        <ul className={s.list}>
-            { contacts.map(({ id, name, number}) => (
-                <li key={id} className={s.listItem} >
-                    <p>{name}: {number}</p>
-                    <button className={s.btnDelete} type="button" onClick={() => deleteContact(id)}>Delete</button>
-                </li>                
-            ))}
-    
-        </ul>
-    );
+export class ContactList extends Component {
+
+componentDidUpdate() {
+    localStorage.setItem('cntcts', JSON.stringify(this.props.contacts));
+}
+
+setContacts = () => {
+    if (JSON.parse(localStorage.getItem('cntcts')).length !== 0) {
+        this.setState.contacts = [...JSON.parse(localStorage.getItem('cntcts'))];
+    } else {
+        this.setState.contacts = [];
+    }    
 };
+
+render() {
+    const { contacts, deleteContact } = this.props;
+    this.setContacts();
+
+return (
+    <ul className={s.list}>
+        {contacts.map(({ id, name, number}) => (
+            <li key={id} className={s.listItem} >
+                <p>{name}: {number}</p>
+                <button className={s.btnDelete} type="button" onClick={() => deleteContact(id)}>Delete</button>
+            </li>                
+        ))}
+
+    </ul>
+)}};
 
 ContactList.propTypes = {
     contacts: PropTypes.array.isRequired,
